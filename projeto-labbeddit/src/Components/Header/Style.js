183f8df3acfb1dd6f77793db5520c2471114dd19
Diego Miyabara <diego.miyabara@hotmail.com>
2020-08-03 17:26:48 -0300

    Estruturação inicial, criação do router, implementação página de login

---
+++ projeto-labbeddit/src/Components/Header/Style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const ContainerHeader = styled.div`
+    display: flex;
+    height: 70px;
+    background-color: #000;
+    color: #FFF;
+    justify-content: space-between;
+    padding: 0 20px;
+`
+
+export const ButtonContainer = styled.div`
+    align-self: center;
+    margin: 10px;
+    width: 150px;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const TituloPrincipal = styled.h1`
+    margin: 0;
+    padding: 0;
+`
+
+export const TituloCointainer = styled.div`
+    align-self: center;
+`
