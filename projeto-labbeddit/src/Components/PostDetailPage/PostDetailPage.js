a3ff24b2c50d72291314edc5fe252acd99b43f19
Diego Miyabara <diego.miyabara@hotmail.com>
2020-08-05 19:02:33 -0300

    feito o ternario para corrigir plural e singular dos comentarios

--- projeto-labbeddit/src/Components/PostDetailPage/PostDetailPage.js
+++ projeto-labbeddit/src/Components/PostDetailPage/PostDetailPage.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import Header from '../Header/Header'
 import { useHistory, useParams } from "react-router-dom";
 import axios from 'axios';
-import {StyledPaper, TituloUsuario, ContainerPost, PostFooter, ContainerButton, TextContainer, LikesContainer, LikesButton, CommentForm, CommentPaper, ContainerCommentText, CommentsTitle, CommentUserContainer, CommentLikesContainer, CommentSection, StyledSVG, Loading} from './Style'
+import {StyledPaper, TituloUsuario, ContainerPost, PostFooter, ContainerButton, TextContainer, LikesContainer, LikesButton, CommentForm, CommentPaper, ContainerCommentText, CommentsTitle, CommentUserContainer, CommentLikesContainer, CommentSection, Loading} from './Style'
 import { TextField, Button } from '@material-ui/core';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
@@ -167,7 +167,7 @@ function PostDetailPage () {
                             <TextContainer>{post.votesCount}</TextContainer>
                             <LikesButton onClick={votePostDown}>{post.userVoteDirection === -1 ? <ArrowDownwardIcon color="secondary"/> : <ArrowDownwardIcon />}</LikesButton>
                         </LikesContainer>
-                        <TextContainer>{post.commentsCount} comentários</TextContainer>
+                        <TextContainer>{post.commentsCount} {post.commentsCount <= 1 ? 'comentário' : 'comentários'}</TextContainer>
                     </PostFooter>
             </StyledPaper>
                 <CommentForm onSubmit={createComment}>
@@ -183,7 +183,7 @@ function PostDetailPage () {
                 </CommentForm>
             <CommentsTitle>Comentários</CommentsTitle>
             <CommentSection>
-            {comments.length === 0 ? <Loading><StyledSVG viewBox="25 25 50 50"><circle cx="50" cy="50" r="20"></circle></StyledSVG></Loading> : comments.map((comment) => {
+            {comments.length === 0 ? <Loading>Não existem comentários neste post!</Loading> : comments.map((comment) => {
                 return(
                     <CommentPaper key={comment.id}>
                         <CommentUserContainer>{comment.username}</CommentUserContainer>
